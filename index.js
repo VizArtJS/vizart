5178d7a88d1679be33757e037e664169ee5df55f
Xing Yun <ink.xing.yun@gmail.com>
2017-07-21 21:28:36 -0400

    Add geo, path and hierarchy
    all at version 0.6.0

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ export { version } from './package.json';
 
 export * from "vizart-core";
 export * from "vizart-basic";
-// export * from "vizart-path";
-// export * from "vizart-hierarchy";
-// export * from "vizart-geo";
+export * from "vizart-path";
+export * from "vizart-hierarchy";
+export * from "vizart-geo";
 //
 export * from 'vizart-basic/dist/vizart-basic.css';
-// export * from 'vizart-path/dist/vizart-path.css';
-// export * from 'vizart-hierarchy/dist/vizart-hierarchy.css';
-// export * from 'vizart-geo/dist/vizart-geo.css';
+export * from 'vizart-path/dist/vizart-path.css';
+export * from 'vizart-hierarchy/dist/vizart-hierarchy.css';
+export * from 'vizart-geo/dist/vizart-geo.css';
