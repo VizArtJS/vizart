a6a2c7841bda52321afc4df5bee870bb4de32e6f
Xing Yun <ink.xing.yun@gmail.com>
2017-07-21 20:42:43 -0400

    add vizard-basic v0.6.0

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 export { version } from './package.json';
 
 export * from "vizart-core";
-// export * from "exceed-vis-basic";
-// export * from "exceed-vis-path";
-// export * from "exceed-vis-hierarchy";
-// export * from "exceed-vis-cluster";
-// export * from "exceed-vis-geo";
-// export * from "exceed-vis-network";
-// export * from "exceed-vis-3d";
+export * from "vizart-basic";
+// export * from "vizart-path";
+// export * from "vizart-hierarchy";
+// export * from "vizart-geo";
 //
-// export * from 'exceed-vis-basic/dist/exceed-vis-basic.css';
-// export * from 'exceed-vis-path/dist/exceed-vis-path.css';
-// export * from 'exceed-vis-hierarchy/dist/exceed-vis-hierarchy.css';
-// export * from 'exceed-vis-cluster/dist/exceed-vis-cluster.css';
-// export * from 'exceed-vis-geo/dist/exceed-vis-geo.css';
-// export * from 'exceed-vis-network/dist/exceed-vis-network.css';
-// export * from 'exceed-vis-3d/dist/exceed-vis-3d.css';
+export * from 'vizart-basic/dist/vizart-basic.css';
+// export * from 'vizart-path/dist/vizart-path.css';
+// export * from 'vizart-hierarchy/dist/vizart-hierarchy.css';
+// export * from 'vizart-geo/dist/vizart-geo.css';
